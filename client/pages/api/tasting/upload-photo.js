77d176a950973ce8f1faf38960a5ec0a2335bdc8
Illouminus <professionnel.baillot@gmail.com>
2023-04-15 23:20:59 +0200

    bucket name

--- client/pages/api/tasting/upload-photo.js
+++ client/pages/api/tasting/upload-photo.js
@@ -17,7 +17,7 @@ export default async (req, res) => {
         let { name, type } = req.body;
         console.log(name, type)
         const fileParams = {
-            Bucket: process.env.BUCKET_NAME,
+            Bucket: "les-amoureuses",
             Key: name,
             Expires: 600,
             ContentType: type,
