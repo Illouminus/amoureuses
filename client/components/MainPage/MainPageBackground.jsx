29a9fcf4b5c4b9a264d2968828a918e38f8e5194
Illouminus <professionnel.baillot@gmail.com>
2023-04-17 19:03:01 +0200

    ading multiple background images

--- client/components/MainPage/MainPageBackground.jsx
+++ client/components/MainPage/MainPageBackground.jsx
@@ -1,16 +1,50 @@
-import cls from './MainPage.module.scss'
+import React, { useState, useEffect } from 'react';
+import cls from './MainPage.module.scss';
+
 export const MainPageBackground = () => {
+
+    const images = [
+        '/img/carousel/01.jpg',
+        '/img/carousel/02.jpg',
+        '/img/carousel/03.jpg',
+        '/img/carousel/04.jpg',
+        '/img/carousel/05.jpg',
+        '/img/carousel/06.jpg',
+        '/img/carousel/08.jpg',
+        // Add more image paths as needed
+    ];
+
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+
+    useEffect(() => {
+        const changeImage = () => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        };
+
+        changeImage();
+
+        const timer = setInterval(() => {
+            changeImage()
+
+        }, 300000); // Change image every 5 minutes
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
     return (
         <div className={cls.divBackground}>
-        <div style={{
-            backgroundImage: `url('/img/Bar.jpg')`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            width: '100%',
-            height: '100%',
-        }} />
+            {images.map((image, index) => (
+                <div
+                    key={image}
+                    className={`${cls.backgroundImage} ${index === currentImageIndex ? cls.active : ''} `}
+                    style={index === currentImageIndex ? { animation: 'fadeInOut 10s linear' } : {}}
+                >
+                    <img src={image} alt={`Background image ${index + 1}`} className={cls.backgroundImageImg} />
+                </div>
+            ))}
         </div>
-    )
-}
-
+    );
+};
