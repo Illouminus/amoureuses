92b13a52d055903228bc17d367fe6b8892428a91
Illouminus <professionnel.baillot@gmail.com>
2023-04-29 09:24:06 +0200

    change nackground carousel

--- client/components/MainPage/MainPageBackground.jsx
+++ client/components/MainPage/MainPageBackground.jsx
@@ -3,46 +3,17 @@ import cls from './MainPage.module.scss';
 
 export const MainPageBackground = () => {
 
-    const images = [
-        '/img/carousel/01.jpg',
-        '/img/carousel/02.jpg',
-        '/img/carousel/03.jpg',
-        '/img/carousel/04.jpg',
-        '/img/carousel/05.jpg',
-        '/img/carousel/06.jpg',
-    ];
-
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-
-    useEffect(() => {
-        const changeImage = () => {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        };
-
-        changeImage();
-
-        const timer = setInterval(() => {
-            changeImage()
-
-        }, 300000); // Change image every 5 minutes
-
-        return () => {
-            clearInterval(timer);
-        };
-    }, []);
 
     return (
         <div className={cls.divBackground}>
-            {images.map((image, index) => (
-                <div
-                    key={image}
-                    className={`${cls.backgroundImage} ${index === currentImageIndex ? cls.active : ''} `}
-                    style={index === currentImageIndex ? { animation: 'fadeInOut 10s linear' } : {}}
-                >
-                    <img src={image} alt={`Background image ${index + 1}`} className={cls.backgroundImageImg} />
-                </div>
-            ))}
+            <div style={{
+                backgroundImage: `url('/img/carousel/03.jpg')`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                width: '100%',
+                height: '100%',
+            }} />
         </div>
     );
 };
