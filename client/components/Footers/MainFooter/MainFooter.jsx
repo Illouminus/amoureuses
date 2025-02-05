e65810b68d3e8940dd43bacd46095bfc8077a762
Illouminus <professionnel.baillot@gmail.com>
2025-02-05 08:52:03 +0100

    change instagram link for mailto href

--- client/components/Footers/MainFooter/MainFooter.jsx
+++ client/components/Footers/MainFooter/MainFooter.jsx
@@ -5,48 +5,48 @@ import mail from '../../../public/img/envelope.svg'
 import telegram from '../../../public/img/chat.svg'
 import Link from "next/link";
 import Modal from "../../Modal";
-import {useState} from "react";
+import { useState } from "react";
 
 export const MainFooter = () => {
-    const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <Modal open={open} handleClose={() => {
-                setOpen(false)
-            }} isContactForm={true}/>
+	return (
+		<>
+			<Modal open={open} handleClose={() => {
+				setOpen(false)
+			}} isContactForm={true} />
 
-            <footer className={cls.footer}>
-                <div className={cls.social}>
-                    <Image
-                        src={mail}
-                        alt="Nous contacter par le formualire de contact"
-                        priority="high"
-                        className={cls.filterColorIcon}
-                        onClick={setOpen}
+			<footer className={cls.footer}>
+				<div className={cls.social}>
+					<Image
+						src={mail}
+						alt="Nous contacter par le formualire de contact"
+						priority="high"
+						className={cls.filterColorIcon}
+						onClick={setOpen}
 
-                    />
-                    <Link href={'https://instagram.com/lesamoureuses.bar?igshid=YmMyMTA2M2Y='} target={"_blank"} ><Image
-                       src={instagram}
-                       alt="Nous suivre sur instargar"
-                       priority="high"
-                       className={cls.filterColorIcon}
-                      />
-                    </Link>
+					/>
+					<Link href={'mailto:pariswinesociety@gmail.com'} target={"_blank"} ><Image
+						src={instagram}
+						alt="Nous suivre sur instargar"
+						priority="high"
+						className={cls.filterColorIcon}
+					/>
+					</Link>
 
 
 
-                    <Link href={'https://t.me/Blueberrrynights'} target={"_blank"} ><Image
-                        src={telegram}
-                        alt="Nous contacter via Telegram"
-                        priority="high"
-                        className={cls.filterColorIcon}
-                    />
-                    </Link>
-                </div>
-            </footer>
-        </>
-    )
+					<Link href={'https://t.me/Blueberrrynights'} target={"_blank"} ><Image
+						src={telegram}
+						alt="Nous contacter via Telegram"
+						priority="high"
+						className={cls.filterColorIcon}
+					/>
+					</Link>
+				</div>
+			</footer>
+		</>
+	)
 }
 
 
